dda45775957d36b8e35f6c9e1fee3a3f1a728013
pingoo06 <berrez.nicolas@gmail.com>
2012-11-12 23:53:41 +0100

    cerrectif final avec l'espace derriere le where

--- Mediatheque/src/java/enterprise/web_jpa_war/dao/impl/mediatheque/item/OeuvreDao.java
+++ Mediatheque/src/java/enterprise/web_jpa_war/dao/impl/mediatheque/item/OeuvreDao.java
@@ -86,7 +86,7 @@ public class OeuvreDao extends AbstractCommonnDao implements ICommonDao<Oeuvre> 
         String retour = DaoTool.analyseParams(mapParamsOeuvre,"o");
         System.out.println("select o from Oeuvre o where" + retour);
         Long tpsAvt = System.currentTimeMillis();
-        List<Oeuvre> result = (List<Oeuvre>) em.createQuery("select o from Oeuvre o where" + retour).getResultList();
+        List<Oeuvre> result = (List<Oeuvre>) em.createQuery("select o from Oeuvre o where " + retour).getResultList();
         System.out.println("Temps de réponse : " + (System.currentTimeMillis() - tpsAvt) + "ms");
         return result;     
     }
