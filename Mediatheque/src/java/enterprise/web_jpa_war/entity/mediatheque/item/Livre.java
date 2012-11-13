a9bcb69cc7c7113c3baa83e2165727d929d107b0
pingoo06 <berrez.nicolas@gmail.com>
2012-11-13 23:29:51 +0100

    merge fichier momo

--- Mediatheque/src/java/enterprise/web_jpa_war/entity/mediatheque/item/Livre.java
+++ Mediatheque/src/java/enterprise/web_jpa_war/entity/mediatheque/item/Livre.java
@@ -4,7 +4,10 @@
  */
 package enterprise.web_jpa_war.entity.mediatheque.item;
 
+import enterprise.web_jpa_war.util.DateTool;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Random;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -56,6 +59,59 @@ public class Livre extends Oeuvre implements Serializable {
         return true;
     }
 
+    public static Livre buildMoke() {
+
+        Livre f = new Livre();
+        f.setDateParution(DateTool.parseDate("2009-06-12"));
+        f.setGenre(Oeuvre.generateRandomGenre());
+        f.setLangue(Oeuvre.generateRandomLangue());
+        f.setTitre(Oeuvre.generateRandomTitle());
+        f.setAuteur(Livre.generateRandomAuteur());
+        f.setEditeur(Livre.generateRandomEditeur());
+        
+        return f;
+    }
+
+    private static String generateRandomAuteur() {
+        ArrayList<String> auteur = new ArrayList<String>();
+
+        auteur.add("kateb yacine");
+        auteur.add("mohamed isso");
+        auteur.add("Michel Dupont");
+        auteur.add("Fred le touriste");
+
+        Random r = new Random();
+
+        String auteuru = auteur.get(r.nextInt(auteur.size()));
+
+        return auteuru;
+    }
+
+    private static String generateRandomEditeur() {
+        ArrayList<String> editeur = new ArrayList<String>();
+
+        editeur.add("Hubert le couvert");
+        editeur.add("Guy Copain");
+        editeur.add("pas d'inspiration");
+        editeur.add("pas envie de réfléchir");
+        editeur.add("cool");
+
+        Random r = new Random();
+
+        String editeuru = editeur.get(r.nextInt(editeur.size()));
+
+        return editeuru;
+    }
+
+    public static ArrayList<Livre> buildMoke(int nb) {
+        ArrayList<Livre> al = new ArrayList<Livre>();
+
+        for (int i = 0; i < nb; i++) {
+            al.add(buildMoke());
+        }
+        return al;
+
+    }
     @Override
     public String toString() {
         return "enterprise.web_jpa_war.entity.mediatheque.item.Livre[ id=" + id + " ]";
